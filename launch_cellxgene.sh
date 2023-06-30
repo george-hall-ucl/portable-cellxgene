fd80b4e1ed2fb4a789858e7cd6265bcd6b447e17
george-hall-ucl <george.hall@ucl.ac.uk>
2023-06-30 18:34:08 +0100

    Use new conda env; make drag/drop; use spoof_cmd.sh

--- launch_cellxgene.sh
+++ launch_cellxgene.sh
@@ -1,6 +1,19 @@
-source cellxgene_env/bin/activate
-export CELLXGENE_DATA="cellxgene_datasets"
-export CELLXGENE_LOCATION="cellxgene_env/bin/cellxgene"
+if [ $# -eq 0 ]
+then
+    echo "Drop a folder here..."
+    exit 0
+fi
+
+DATA_DIR=$1
+
+source cellxgene_portable_conda_env/bin/activate
+
+export CELLXGENE_DATA=$DATA_DIR
+export CELLXGENE_LOCATION="./spoof_cmd.sh"
+export GATEWAY_ENABLE_ANNOTATIONS=1
+export GATEWAY_LOG_LEVEL="WARNING" # Decrease verbosity
+
 sleep 1 && open http://127.0.0.1:5005/filecrawl.html &
 cellxgene-gateway
-source cellxgene_env/bin/deactivate
+
+source cellxgene_portable_conda_env/bin/deactivate
