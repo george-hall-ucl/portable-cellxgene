7252f154001df94ba95fc8db496a3032101fa966
george-hall-ucl <george.hall@ucl.ac.uk>
2023-07-05 16:22:56 +0100

    Use GATEWAY_ENABLE_GENE_SETS=1 instead of spoof command

--- launch_cellxgene.sh
+++ launch_cellxgene.sh
@@ -9,8 +9,9 @@ DATA_DIR=$1
 source cellxgene_portable_conda_env/bin/activate
 
 export CELLXGENE_DATA=$DATA_DIR
-export CELLXGENE_LOCATION="./spoof_cmd.sh"
+export CELLXGENE_LOCATION="cellxgene_portable_conda_env/bin/cellxgene"
 export GATEWAY_ENABLE_ANNOTATIONS=1
+export GATEWAY_ENABLE_GENE_SETS=1
 export GATEWAY_LOG_LEVEL="WARNING" # Decrease verbosity
 
 sleep 1 && open http://127.0.0.1:5005/filecrawl.html &
--- spoof_cmd.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-CSV_PATH=$(echo $@ | grep -o "\S*csv")
-CSV_PATH_NO_EXT=${CSV_PATH%????}
-ANNOTATIONS_FILE_NAME=$CSV_PATH_NO_EXT"_annotations.csv"
-NEW_CMD="cellxgene_portable_conda_env/bin/cellxgene ""${@//--annotations-file/--annotations-file $ANNOTATIONS_FILE_NAME --gene-sets-file}"
-$NEW_CMD
